5fa9a69ea8566c19a3dfad9a3bbf3107610da6d9
amymc <amy.mccarthy@hmhco.com>
2018-05-05 18:59:21 +0100

    feat: load new video on click

--- main/App.js
+++ main/App.js
@@ -12,12 +12,15 @@ class App extends Component {
     this.getPlaylist();
   };
 
+  getId = uri => uri.split('/videos/')[1];
+
   getPlaylist = () => {
     fetch(
       'https://api.vimeo.com/channels/documentary/videos?access_token=b01e6d1ea960f10c41ed22b14baa8c07'
     )
       .then(resp => resp.json())
       .then(({ data }) => {
+        data.forEach(item => (item.id = this.getId(item.uri)));
         this.setState({ currentVideo: data[0], videos: data.slice(1) });
       });
   };
--- main/Player.js
+++ main/Player.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import VimeoPlayer from '@vimeo/player';
 
 class Player extends Component {
-  componentDidUpdate(prevProps) {
-    const { video } = this.props;
-    if (prevProps.video !== video) {
-      new VimeoPlayer('player', {
-        url: video.link,
+  componentDidUpdate = prevProps => {
+    const { player, props } = this;
+    if (prevProps.video === props.video) return;
+    if (player) {
+      player.loadVideo(props.video.id);
+    } else {
+      this.player = new VimeoPlayer('player', {
+        id: props.video.id,
         width: 640,
       });
     }
-  }
+  };
+
+  componentWillUnmount = () => {
+    this.player.destroy();
+  };
 
   render() {
     return <div id="player" />;
